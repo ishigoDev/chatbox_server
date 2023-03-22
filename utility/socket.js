1ad416ce94683e7d264daffbef8833e6643fce7e
pranaySpark18 <evincedevapi@gmail.com>
2023-03-22 17:48:43 +0530

    feature/added the active user support

--- utility/socket.js
+++ utility/socket.js
@@ -1,17 +1,38 @@
+
 let io;
-function init(http){
-    io = require('socket.io')(http, {
+let activeUsers = [];
+const db = require('../models/index')
+const user = db.user;
+
+function init(http){    
+    io = require('socket.io')(http, {        
         cors: {
           origin: '*',
           methods: ['GET', 'POST'],
           credentials: true,
         },
-        allowEIO3: true, // false by default
+        allowEIO3: true,
     })
     io.on('connection', (socket) => {
-        console.log('a user connected');
+        socket.on('new-user-add', async (newUserId) => {
+            const userDetail = await user.findByPk(newUserId) 
+            if(!activeUsers.some(user => user.id === newUserId)){                             
+                activeUsers.push({
+                    id:newUserId,
+                    name: userDetail.dataValues.displayName,
+                    socketId:socket.id
+                })
+            }
+            io.emit('get-users',activeUsers)
+        })
+        socket.on('disconnect', () => {
+            activeUsers = activeUsers.filter(user => user.socketId!== socket.id)
+            console.log('user disconnected',activeUsers);
+            io.emit('get-users',activeUsers)
+        })
     });    
 }
+
 
 module.exports ={
     init
