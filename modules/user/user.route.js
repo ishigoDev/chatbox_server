e6aec81050d29eb15ff9264239746b87ba012354
pranaySpark18 <evincedevapi@gmail.com>
2023-04-22 14:42:38 +0530

    feature/added the signout api

--- modules/user/user.route.js
+++ modules/user/user.route.js
@@ -8,5 +8,7 @@ router.post('/create',rule.userBody,rule.verifyRules,userController.create)
 router.post('/sign-in',rule.userSign,rule.verifyRules,userController.signin);
 //ChatRoom 
 router.get('/chatroom',auth.verifyToken,userController.chatroom)
+//Sign Out
+router.get('/sign-out',auth.verifyToken,userController.signOut)
 
 module.exports = router;
